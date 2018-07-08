1fa0962b40a370b6d4f7c2b1490be092eec2313b
mifsudm <mifsudm@unimelb.edu.au>
2018-07-09 08:06:14 +1000

    Minor Code Updates - 252s-dev

---
+++ Tk/Crumbs.php
@@ -0,0 +1,288 @@
+<?php
+namespace Tk;
+
+
+/**
+ * Use this object to track and render a crumb stack
+ *
+ * See the controlling object \Uni\Listeners\CrumbsHandler to
+ * view its implementation.
+ *
+ *
+ * @author Michael Mifsud <info@tropotek.com>
+ * @see http://www.tropotek.com/
+ * @license Copyright 2016 Michael Mifsud
+ */
+class Crumbs extends \Dom\Renderer\Renderer
+{
+    /**
+     * Request param: Reset the crumb stack
+     */
+    const CRUMB_RESET = 'crumb_reset';
+    /**
+     * Request param: Do not add the current URI to the crumb stack
+     */
+    const CRUMB_IGNORE = 'crumb_ignore';
+
+    /**
+     * @var string
+     */
+    public static $homeUrl = '/index.html';
+
+    /**
+     * @var string
+     */
+    public static $homeTitle = 'Home';
+
+
+    /**
+     * @var Crumbs
+     */
+    public static $instance = null;
+
+    /**
+     * @var array
+     */
+    protected $list = array();
+
+    /**
+     * @var \Tk\Session
+     */
+    protected $session = null;
+
+
+    /**
+     * @param \Tk\Session $session
+     */
+    protected function __construct($session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param \Tk\Session $session
+     * @return static
+     */
+    static protected function create($session)
+    {
+        $obj = new static($session);
+        return $obj;
+    }
+
+    /**
+     * @param null|\Tk\Session $session
+     * @return Crumbs
+     */
+    public static function getInstance($session = null)
+    {
+        if (!$session)
+            $session = \Tk\Config::getInstance()->getSession();
+
+        if (!self::$instance) {
+            $crumbs = self::create($session);
+            if ($session->has($crumbs->getSid())) {
+                $crumbs->setList($session->get($crumbs->getSid()));
+            }
+            if (!count($crumbs->getList())) {
+                $crumbs->addCrumb(self::$homeTitle, \Tk\Uri::create(self::$homeUrl));
+            }
+            self::$instance = $crumbs;
+        }
+        return self::$instance;
+    }
+
+    /**
+     * @param string $homeTitle
+     * @param null|\Tk\Uri $url
+     * @return null|Crumbs If null returned then the crumbs were not reset
+     * @throws \Exception
+     */
+    public static function reset($homeTitle = 'Dashboard', $url = null)
+    {
+        $crumbs = self::getInstance();
+        if ($crumbs && !\Tk\Config::getInstance()->getRequest()->has(self::CRUMB_IGNORE)) {
+            if (!$url) {
+                $homeTitle = self::$homeTitle;
+                $url = \Tk\Uri::create(self::$homeUrl);
+            }
+            $crumbs->getSession()->remove($crumbs->getSid());
+            $crumbs->setList();
+            $crumbs->addCrumb($homeTitle, $url);
+            $crumbs->save();
+            return $crumbs;
+        }
+    }
+
+
+    /**
+     * save the state of the crumb stack to the session
+     */
+    public static function save()
+    {
+        $crumbs = self::getInstance();
+        if ($crumbs) {
+            $crumbs->getSession()->set($crumbs->getSid(), $crumbs->getList());
+        }
+    }
+
+    /**
+     * Get the crumbs session ID
+     *
+     * @return string
+     */
+    public function getSid()
+    {
+        return 'crumbs.' . self::$homeUrl;
+    }
+
+    /**
+     * @return \Tk\Session
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
+
+    /**
+     * Get teh crumb list
+     *
+     * @return array
+     */
+    public function getList()
+    {
+        return $this->list;
+    }
+
+    /**
+     * Use to restore crumb list.
+     * format:
+     *   array(
+     *     'Page Name' => '/page/url/pageUrl.html'
+     *   );
+     *
+     * @param $list
+     */
+    public function setList($list = array())
+    {
+        $this->list = $list;
+    }
+
+    /**
+     * @return \Tk\Uri
+     */
+    public function getBackUrl()
+    {
+        $url = '';
+        if (count($this->list) == 1) {
+            $url = end($this->list);
+        } if (count($this->list) > 1) {
+        end($this->list);
+        $url = prev($this->list);
+    }
+        return \Tk\Uri::create($url);
+    }
+
+    /**
+     * @param string $title
+     * @param \Tk\Uri|string $url
+     * @return $this
+     */
+    public function addCrumb($title, $url)
+    {
+        $url = \Tk\Uri::create($url);
+        $this->list[$title] = $url->toString();
+        return $this;
+    }
+
+    /**
+     * @param string $title
+     * @param \Tk\Uri|string $url
+     * @return $this
+     */
+    public function replaceCrumb($title, $url) {
+        array_pop($this->list);
+        return $this->addCrumb($title, $url);
+    }
+
+    /**
+     * @param $title
+     * @return array
+     */
+    public function trimByTitle($title) {
+        $l = array();
+        foreach ($this->list as $t => $u) {
+            if ($title == $t) break;
+            $l[$t] = $u;
+        }
+        $this->list = $l;
+        return $l;
+    }
+
+    /**
+     * @param $url
+     * @param bool $ignoreQuery
+     * @return array
+     */
+    public function trimByUrl($url, $ignoreQuery = true) {
+        $url = \Tk\Uri::create($url);
+        $l = array();
+        foreach ($this->list as $t => $u) {
+            if ($ignoreQuery) {
+                if (\Tk\Uri::create($u)->getRelativePath() == $url->getRelativePath()) {
+                    break;
+                }
+            } else {
+                if (\Tk\Uri::create($u)->toString() == $url->toString()) {
+                    break;
+                }
+            }
+            $l[$t] = $u;
+        }
+        $this->list = $l;
+        return $l;
+    }
+
+    /**
+     * @return \Dom\Template
+     */
+    public function show()
+    {
+        $template = $this->getTemplate();
+
+        $i = 0;
+        foreach ($this->list as $title => $url) {
+            $repeat = $template->getRepeat('li');
+            if (!$repeat) continue;         // ?? why and how does the repeat end up null.
+            if ($i < count($this->list)-1) {
+                $repeat->setAttr('url', 'href', \Tk\Uri::create($url)->toString());
+                $repeat->insertText('url', $title);
+            } else {    // Last item
+                $repeat->insertText('li', $title);
+                $repeat->addCss('li','active');
+            }
+
+            $repeat->appendRepeat();
+            $i++;
+        }
+
+        return $template;
+    }
+
+    /**
+     * DomTemplate magic method
+     *
+     * @return \Dom\Template
+     */
+    public function __makeTemplate()
+    {
+        $html = <<<HTML
+<ol class="breadcrumb" var="breadcrumb">
+  <li repeat="li" var="li"><a href="#" var="url"></a></li>
+</ol>
+HTML;
+
+        return \Dom\Loader::load($html);
+    }
+
+
+}
---
+++ Tk/Listener/CrumbsHandler.php
@@ -0,0 +1,107 @@
+<?php
+namespace Tk\Listener;
+
+use Tk\Event\Subscriber;
+use Tk\Kernel\KernelEvents;
+
+/**
+ * @author Michael Mifsud <info@tropotek.com>
+ * @see http://www.tropotek.com/
+ * @license Copyright 2015 Michael Mifsud
+ */
+class CrumbsHandler implements Subscriber
+{
+    /**
+     * @var bool
+     */
+    protected $renderEnabled = true;
+
+    /**
+     * @param bool $renderEnabled Set this to false to create your own onShow() Handlers
+     */
+    public function __construct($renderEnabled = true)
+    {
+        $this->renderEnabled = $renderEnabled;
+    }
+
+
+
+    
+    /**
+     * @param \Tk\Event\ControllerEvent $event
+     * @throws \Tk\Exception
+     */
+    public function onController(\Tk\Event\ControllerEvent $event)
+    {
+        $crumbs = \Tk\Ui\Crumbs::getInstance();
+        if (!$crumbs) throw new \Tk\Exception('Error creating crumb instance.');
+
+        /** @var \Tk\Controller\Iface $controller */
+        $controller = $event->getController();
+        if ($controller instanceof \Tk\Controller\Iface) {
+            // ignore adding crumbs if param in request URL
+            if ($controller->getRequest()->has(\Tk\Ui\Crumbs::CRUMB_IGNORE)) {
+                return;
+            }
+            $title = $controller->getPageTitle();
+            $crumbs->trimByTitle($title);
+            $crumbs->addCrumb($title, \Tk\Uri::create());
+        }
+    }
+
+    /**
+     * @param \Tk\Event\Event $event
+     */
+    public function onShow(\Tk\Event\Event $event)
+    {
+        if (!$this->renderEnabled) return;
+        $controller = $event->get('controller');
+        /** @var \Tk\Controller\Page $page */
+        $page = $controller->getPage();
+
+        if ($page instanceof \Tk\Controller\Page) {
+            $crumbs = \Tk\Ui\Crumbs::getInstance();
+            if (!$crumbs) return;
+
+            $template = $page->getTemplate();
+            $backUrl = $crumbs->getBackUrl();
+            $js = <<<JS
+config.backUrl = '$backUrl';
+JS;
+            $template->appendjs($js, array('data-jsl-priority' => '-999'));
+
+            $js = <<<JS
+jQuery(function($) {
+  $('a.btn.back').attr('href', config.backUrl);
+});
+JS;
+            $template->appendjs($js);
+            if ($template->keyExists('var', 'breadcrumb')) {
+                $template->replaceTemplate('breadcrumb', $crumbs->show());
+                $template->setChoice('breadcrumb');
+            }
+        }
+    }
+
+    /**
+     * @param \Tk\Event\RequestEvent $event
+     */
+    public function onFinishRequest(\Tk\Event\RequestEvent $event)
+    {
+        $crumbs = \Tk\Ui\Crumbs::getInstance();
+        $crumbs->save();
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::CONTROLLER => array('onController', 11),
+            \Tk\PageEvents::CONTROLLER_SHOW =>  array('onShow', 0),
+            KernelEvents::FINISH_REQUEST => 'onFinishRequest'
+        );
+    }
+
+}
