a44117511a771c7922925d8fb3e83f64fe27c3df
godar <info@tropotek.com>
2018-07-27 09:44:28 +1000

    Added new cube template and updated action panel

---
+++ Tk/Listener/ActionPanelHandler.php
@@ -0,0 +1,48 @@
+<?php
+namespace Tk\Listener;
+
+use Tk\Event\Subscriber;
+use Tk\Kernel\KernelEvents;
+
+/**
+ * @author Michael Mifsud <info@tropotek.com>
+ * @see http://www.tropotek.com/
+ * @license Copyright 2015 Michael Mifsud
+ */
+class ActionPanelHandler implements Subscriber
+{
+
+
+    /**
+     * @param \Tk\Event\Event $event
+     * @throws \Dom\Exception
+     */
+    public function onShowController(\Tk\Event\Event $event)
+    {
+        /** @var \Bs\Controller\Iface $controller */
+        $controller = $event->get('controller');
+        if (!$controller instanceof \Tk\Controller\Iface) return;
+        if (!method_exists($controller, 'getActionPanel')) return;
+
+        $pageTemplate = $controller->getPage()->getTemplate();
+        $contentVar = $controller->getPage()->getContentVar();
+        if (!$contentVar)
+            $contentVar = 'content';
+
+        /** @var \Tk\Ui\Admin\ActionPanel $actionPanel */
+        $actionPanel = $controller->getActionPanel();
+        if ($actionPanel->isEnabled() && $actionPanel->isVisible())
+            $pageTemplate->prependTemplate($contentVar, $actionPanel->show());
+
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            \Tk\PageEvents::CONTROLLER_SHOW => array('onShowController', 0)
+        );
+    }
+}
