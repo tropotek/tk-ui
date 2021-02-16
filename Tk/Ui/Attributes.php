95829332e2f5e770d22311a57ed11d1b8406c86e
tropotek <info@tropotek.com>
2021-02-16 17:06:35 +1100

    ~Auto: Commit

---
+++ Tk/Ui/Attributes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tk\Ui;
+
+/**
+ * Class Attributes
+ *
+ * @package Tk\Ui
+ */
+class Attributes
+{
+    use \Tk\Dom\AttributesTrait;
+    
+}
---
+++ Tk/Ui/Css.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tk\Ui;
+
+/**
+ * Class Css
+ *
+ * @package Tk\Ui
+ */
+class Css
+{
+    use \Tk\Dom\CssTrait;
+
+}
